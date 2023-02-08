be519c118eabc8e6ef75297ec3b60fdba2b4e7dd
HafsaI <hafsairfan80@gmail.com>
2023-02-08 22:47:15 +0500

    data from db - onto website

--- src/components/History.js
+++ src/components/History.js
@@ -1,6 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState, useEffect } from "react";
+import app from "../firebaseconfig";
+import { getFirestore } from "@firebase/firestore";
+
+import {
+  collection, getDocs,
+} from "firebase/firestore";
+
 
 function History(){
+  const db = getFirestore(app);
+
+
+  const [users, setUsers] = useState([]);
+  const [userSession, setUserSessionScores] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  const scoresCollectionRef = collection(db, "training_sessions");
+
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    const getUserSessionScores = async () => {
+      const data = await getDocs(scoresCollectionRef);
+      setUserSessionScores(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+    getUserSessionScores();
+  }, []);
 
   return (
     <div class="wrapper">
@@ -17,6 +48,31 @@ function History(){
           <label for="tab-2" class="tab-label">Overall Progress Report</label>
           <div class="tab-content">
             <h4>Listenability</h4>
+            {users.map((user) => {
+              return (
+              
+              <div> 
+               <p> Name: {user.username}</p>
+               <p> User ID: {user.id}</p>
+              
+              </div>
+              )
+            })}
+             {userSession.map((session) => {
+              return (
+              
+              <div> 
+               <p> Clarity: {session.clarity_score}</p>
+               <p>Speaking rate: {session.speaking_rate_score}</p>
+              
+              </div>
+              )
+            })}
+
+
+
+
+
           </div>
         </div>
       </div>
