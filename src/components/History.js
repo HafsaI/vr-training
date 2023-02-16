6a297dae3bf4a6e7851f363eb2bc6ea4f8086509
batool-ahmed <ba06180@st.habib.edu.pk>
2023-02-16 17:25:47 +0000

    minor update

--- src/components/History.js
+++ src/components/History.js
@@ -30,14 +30,14 @@ function History(){
     <div class="wrapper">
       <div class="tabs">
         <div class="tab" id='divTab1'>
-          <input type="radio" name="css-tabs" id="tab-1" checked class="tab-switch"/>
+          <input type="radio" name="css-tabs" id="tab-1" class="tab-switch"/>
           <label for="tab-1" class="tab-label">Overall Progress Report</label>
           <div class="tab-content">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis orci sed dui placerat efficitur a at lorem. Suspendisse feugiat, massa in placerat tincidunt, augue urna tempor urna, vitae congue sapien massa in leo. Nunc id molestie tellus, ac volutpat enim. Nam at pulvinar purus. Vestibulum cursus tempus turpis sit amet porta. Aenean mattis felis eget dui porttitor placerat. Sed facilisis feugiat nunc ut aliquam. Nunc cursus mattis rutrum. Etiam condimentum a ex vel cursus.
           </div>
         </div>
         <div class="tab">
-          <input type="radio" name="css-tabs" id="tab-2" class="tab-switch"/>
+          <input type="radio" name="css-tabs" id="tab-2" checked class="tab-switch"/>
           <label for="tab-2" class="tab-label">Last Session Report</label>
           <div class="tab-content">
             {userSession.map((session) => {
