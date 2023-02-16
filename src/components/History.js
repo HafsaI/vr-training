a1c409aedf74beb5b784a2966b6ff4becd60ff4e
batool-ahmed <ba06180@st.habib.edu.pk>
2023-02-16 17:30:15 +0000

    colons removed

--- src/components/History.js
+++ src/components/History.js
@@ -49,26 +49,26 @@ function History(){
               <div> 
               <h5>Speech Analysis</h5>
               <hr/>
-              <p> Clarity: <span className='rightAlign'>{session.clarity_score}/10</span></p>
+              <p> Clarity <span className='rightAlign'>{session.clarity_score}/10</span></p>
               <hr/>
-              <p> Pauses: <span className='rightAlign'>{session.pauses_score}/10</span></p>
+              <p> Pauses <span className='rightAlign'>{session.pauses_score}/10</span></p>
               <hr/>
-              <p> Pronunciation: <span className='rightAlign'>{session.pronunciation_score}/10</span></p>
+              <p> Pronunciation <span className='rightAlign'>{session.pronunciation_score}/10</span></p>
               <hr/>
-              <p> SpeakingRate: <span className='rightAlign'>{session.speakingrate_score}/10</span></p>
+              <p> SpeakingRate <span className='rightAlign'>{session.speakingrate_score}/10</span></p>
               <hr/>
-              <p> Listenability: <span className='rightAlign'>{session.listenability_score}/10</span></p>
+              <p> Listenability <span className='rightAlign'>{session.listenability_score}/10</span></p>
               <hr/>
               <h5 className='analysisType'>Body Language Analysis</h5>
               <hr/>
-              <p> Posture: <span className='rightAlign'>{session.posture_score}/10</span></p>
+              <p> Posture <span className='rightAlign'>{session.posture_score}/10</span></p>
               <hr/>
-              <p> Gesture: <span className='rightAlign'>{session.gesture_score}/10</span></p>
+              <p> Gesture <span className='rightAlign'>{session.gesture_score}/10</span></p>
               <hr/>
-              <p> Movement: <span className='rightAlign'>{session.movement_score}/10</span></p>
+              <p> Movement <span className='rightAlign'>{session.movement_score}/10</span></p>
               <h5 className='analysisType'>Nervousness Analysis</h5>
               <hr/>
-              <p> Nervousness: <span className='rightAlign'>{session.nervousness_score}/10</span></p>
+              <p> Nervousness <span className='rightAlign'>{session.nervousness_score}/10</span></p>
              
              </div>
               : null
