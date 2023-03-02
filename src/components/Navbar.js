0fb48024e8af88f102260ef6d40d04e5b56786c0
batool-ahmed <ba06180@st.habib.edu.pk>
2023-03-02 11:18:12 +0000

    webcam without styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route, Redirect, Link } from "react-router-dom";
 import History from './History'
 import Home from './Home'
 import SignIn from './SignIn'
+import WebcamVideo from './WebcamVideo';
 
 function Navbar(){    
   return (
@@ -10,11 +11,13 @@ function Navbar(){
       <BrowserRouter>
           <nav className="nab">
               <a><Link to='/' target='_self'>Home</Link></a>
+              <a><Link to='/record' target='_self'>Record Video</Link></a>
               <a className ="linkHistory"><Link to='/history' target='_self'>History</Link></a>
               <a className="linkLogin"><Link to='/login' target='_self'>Login</Link></a>
           </nav>
           <Routes>
               <Route exact path='/' element={<Home/>}></Route>
+              <Route exact path='/record' element={<WebcamVideo/>}></Route>
               <Route exact path='/history' element={<History/>}></Route>
               <Route exact path='/login' element={<SignIn/>}></Route>
           </Routes>
