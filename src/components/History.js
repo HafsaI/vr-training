e22dfb0cb1e7d7b202a25a6757de558ad347949b
batool-ahmed <ba06180@st.habib.edu.pk>
2023-03-07 17:41:07 +0000

    separating scores from history component

--- src/components/History.js
+++ src/components/History.js
@@ -6,6 +6,7 @@ import {
   collection, getDocs,} from "firebase/firestore";
 import { LoginContext} from "../AppContext/Context";
 import LineChart from './LineChart';
+import Scores from './Scores';
 
 // TODO: rename everything of 'history' to report
 
@@ -43,35 +44,8 @@ function History(){
           <div class="tab-content">
             {userSession.map((session) => {
               return ( 
-
               session.user_id === user?.uid?
-
-              // batool put all scores in a component and call a component here that does all this and give key to component to solve the warning error 
-              <div> 
-              <h5>Speech Analysis</h5>
-              <hr/>
-              <p> Clarity <span className='rightAlign'>{session.clarity_score}/10</span></p>
-              <hr/>
-              <p> Pauses <span className='rightAlign'>{session.pauses_score}/10</span></p>
-              <hr/>
-              <p> Pronunciation <span className='rightAlign'>{session.pronunciation_score}/10</span></p>
-              <hr/>
-              <p> SpeakingRate <span className='rightAlign'>{session.speakingrate_score}/10</span></p>
-              <hr/>
-              <p> Listenability <span className='rightAlign'>{session.listenability_score}/10</span></p>
-              <hr/>
-              <h5 className='analysisType'>Body Language Analysis</h5>
-              <hr/>
-              <p> Posture <span className='rightAlign'>{session.posture_score}/10</span></p>
-              <hr/>
-              <p> Gesture <span className='rightAlign'>{session.gesture_score}/10</span></p>
-              <hr/>
-              <p> Movement <span className='rightAlign'>{session.movement_score}/10</span></p>
-              <h5 className='analysisType'>Nervousness Analysis</h5>
-              <hr/>
-              <p> Nervousness <span className='rightAlign'>{session.nervousness_score}/10</span></p>
-             
-             </div>
+              <Scores session = {session}/>
               : null
               )
 
