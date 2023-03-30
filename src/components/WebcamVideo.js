9a112101dc1fe184eadf35633538e7a34f8081d4
Haania Siddiqui <59687296+HaaniaSiddiqui@users.noreply.github.com>
2023-03-30 21:58:20 +0500

    Automatic Start and Stop

--- src/components/WebcamVideo.js
+++ src/components/WebcamVideo.js
@@ -6,7 +6,7 @@ import app from "../firebaseconfig";
 import { v4 } from "uuid";
 import { LoginContext} from "../AppContext/Context";
 import { getFirestore } from "@firebase/firestore";
-import { collection, getDocs, getDoc, updateDoc,doc, onSnapshot} from "firebase/firestore";
+import {  collection, getDocs, getDoc, updateDoc,doc, onSnapshot} from "firebase/firestore";
 
 
 export default function WebcamVideo() {
@@ -21,51 +21,20 @@ export default function WebcamVideo() {
   const [liveSession, setliveSession] = useState([])
   // const [lastSession, setlastSession] = useState([])
   const currSessidRef = useRef({currSessId: ''});
-
-
-  useEffect(() => {
-    const db = getFirestore(app);
-    const scoresCollectionRef = collection(db, "training_sessions");
-    const getAllSessionScores = async () => {
-      const data = await getDocs(scoresCollectionRef);
-      setAllSessionScores(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); 
-    };
-    
-    getAllSessionScores();
-
-  }, []);
-
-
-  useEffect(() => {
-    //gets data of user that is logged in and latest training session id
-    const docRef = doc(db, "users", user?.uid)
-    getDoc(docRef)
-    .then((doc) => {
-      currSessidRef.current.currSessId = doc.data().currSessionId;
-    })
-    
-    if (JSON.stringify(currSessidRef.current.currSessId) !== '[]' && currSessidRef.current.currSessId!= false ){
-
-      const unsub = onSnapshot(doc(db, "training_sessions", currSessidRef.current.currSessId), (doc) => {
-        console.log("[Record] Current Session Value: ", doc.data().session)
-        setliveSession(doc.data().session)
-        // setlastSession(doc.data())
-      })
-     
-    }
-
-    }, [currSessidRef.current.currSessId, liveSession]);
+   
 
   const handleDataAvailable = useCallback(
-    ({ data }) => {
+    ({ data}) => {
       if (data.size > 0) {
         setRecordedChunks((prev) => prev.concat(data));
+        // setRecordedChunks((prev) => [...prev, data]);
+
       }
     },
     [setRecordedChunks]
   );
-
   const handleStartCaptureClick = useCallback(() => {
+    if(webcamRef.current.stream){
     setCapturing(true);
     console.log('start');
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
@@ -75,7 +44,7 @@ export default function WebcamVideo() {
       "dataavailable",
       handleDataAvailable
     );
-    mediaRecorderRef.current.start();
+    mediaRecorderRef.current.start();}
   }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
 
 
@@ -85,6 +54,8 @@ export default function WebcamVideo() {
     // console.log("currs", currSessidRef.current.currSessId)
     setCapturing(false); 
     /* Downloading */
+    console.log('recordedChunks:', recordedChunks);
+
     const blob = new Blob(recordedChunks, {
       type: "video/mp4",
     });
@@ -137,6 +108,88 @@ export default function WebcamVideo() {
     height: 300,
     facingMode: "user",
   };
+  // useEffect(()=>{
+  //   console.log("liveSession", liveSession)
+  //   if(liveSession==false){
+  //     // fetch("http://127.0.0.1:5001/data/")
+  //     //         .then((response) => response.json())
+  //     //         .then((data) => {
+  //     //           // Do something with the response data here
+  //     //           console.log(data);
+  //     //         });
+  //     fetch("http://127.0.0.1:5001/data/", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: JSON.stringify({ value: currSessidRef.current.currSessId })
+  //     });
+  //       // .then((response) => response.json())
+  //       // .then((data) => {
+  //       //   // Do something with the response data here
+  //       //   console.log(data);
+  //       // });
+  //   }
+  //  }, [liveSession]);
+  useEffect(() => {
+    const db = getFirestore(app);
+    const scoresCollectionRef = collection(db, "training_sessions");
+    const getAllSessionScores = async () => {
+      const data = await getDocs(scoresCollectionRef);
+
+      setAllSessionScores(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); 
+    };
+    
+    getAllSessionScores();
+    
+  }, []);
+
+
+  useEffect(() => {
+    //gets data of user that is logged in and latest training session id
+    console.log("user", user)
+    console.log("user?.uid", user?.uid)
+    const docRef = doc(db, "users", user?.uid)
+    getDoc(docRef)
+    .then((d) => {
+      console.log("doc.data().currSessionId",d.data().currSessionId)
+      currSessidRef.current.currSessId = d.data().currSessionId;
+
+      if (JSON.stringify(currSessidRef.current.currSessId) !== '[]' && currSessidRef.current.currSessId!= false ){
+
+        const unsub = onSnapshot(doc(db, "training_sessions", currSessidRef.current.currSessId), (doc) => {
+          console.log("[Record] Current Session Value: ", doc.data().session)
+          setliveSession(doc.data().session)
+          // console.log("A", doc.data().session)
+          // setlastSession(doc.data())
+        }) 
+      }
+    })
+    }, [currSessidRef.current.currSessId, liveSession]);
+
+
+
+
+
+
+
+  
+  
+  
+
+useEffect(() => {
+      console.log("LIVESESSION", liveSession)
+      if (liveSession && !capturing && webcamRef.current.stream) {
+        handleStartCaptureClick();
+      } else if (!liveSession && capturing) {
+        console.log("here")
+        handleStopCaptureClick();
+      }
+    }, [webcamRef, liveSession, handleStartCaptureClick, handleStopCaptureClick]);
+
+  
+
+  
 
   return (
     <div className="Container centerText">
@@ -147,11 +200,11 @@ export default function WebcamVideo() {
         videoConstraints={videoConstraints}
       />
       {/* liveSession variable stores session value for the current training Sess that is going on */}
-      {capturing ? (
+      {/* {capturing ? (
         <button onClick={handleStopCaptureClick}>Stop Capture</button>
       ) : (
         <button onClick={handleStartCaptureClick}>Start Capture</button>
-      )}
+      )} */}
       
     </div>
   );
