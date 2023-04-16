3a95de5fb6d384cd6fa18e9c8ee956a9e869ec15
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-16 23:55:30 +0500

    hover without text

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,26 +1,10 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 function Feature({ img, title, description }) {
-  const overlayRef = useRef(null);
-
-  function handleMouseEnter() {
-    overlayRef.current.style.opacity = '1';
-  }
-
-  function handleMouseLeave() {
-    overlayRef.current.style.opacity = '0';
-  }
-
   return (
     <div className='feature'>
-      <img
-        src={img}
-        className='feature-img'
-        width='400px'
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      />
-      <div className='feature-overlay' ref={overlayRef}>
+      <img src={img} className='feature-img' width="400px"/>
+      <div className='feature-overlay'>
         <h6>{title}</h6>
         <p>{description}</p>
       </div>
