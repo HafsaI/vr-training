71b33682c8f4addee6609525bd64634c9a7866d9
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-16 11:12:01 +0500

    navbar added logo + name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,149 @@
-import React from 'react'
-import {useContext} from "react";
-import {BrowserRouter, Routes, Route, Redirect, Link } from "react-router-dom";
-import Report from './Report'
-import Home from './Home'
-import SignIn from './SignIn'
+import React from 'react';
+import { useContext } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Report from './Report';
+import Home from './Home';
+import SignIn from './SignIn';
 import WebcamVideo from './WebcamVideo';
 import SignUp from './SignUp';
-import { LoginContext,  UserContext} from "../AppContext/Context";
-import SignOut from './SignOut'
+import { LoginContext, UserContext } from "../AppContext/Context";
+import SignOut from './SignOut';
 
-import profilePic from '../../src/images/profile_default.png'
+import profilePic from '../../src/images/profile_default.png';
 
+function Navbar() {
+  const { user, setUser } = useContext(LoginContext);
+  const { userdoc, setUserDoc } = useContext(UserContext);
 
-function Navbar(){
-  const {user,setUser} = useContext(LoginContext); 
-  const {userdoc,setUserDoc} = useContext(UserContext);
   const LoginBtn = (
-    <button className="btnLogin linkLogin"><Link to='/login' target='_self' className='white'>Login</Link></button>
-  )
+    <button className="btnLogin linkLogin">
+      <Link to='/login' target='_self' className='white'>Login</Link>
+    </button>
+  );
 
   const toggleMenu = () => {
     let subMenu = document.getElementById('subMenu');
-    subMenu.classList.toggle("open-menu")
-  }
+    subMenu.classList.toggle("open-menu");
+  };
 
-  // TODO: [Batool] add Empowered on left hand side of navbar 
-  // Problem coming on the above todo
   return (
     <div className='navbar-wrapper'>
-      {/* <span className='navbar-heading'>Empowered</span> */}
       <BrowserRouter>
-          <nav className="nab">
-              <a className='nab-item'><Link to='/' target='_self'>Home</Link></a>
-              { JSON.stringify(user) !== '{}' && user != null && <a className='nab-item'><Link to='/record' target='_self'>Record</Link></a>}
-              { JSON.stringify(user) !== '{}' && user != null && <a className='nab-item'><Link to='/report' target='_self'>Report</Link></a>}
-              {/* { console.log("[Navbar] user: ", user)} */}
-              {/* { console.log(" JSON.stringify(user) === '{}",  JSON.stringify(user) === '{}')} */}
-              { JSON.stringify(user) === '{}' || user == null || user === Object? LoginBtn:null }
-              
-              {JSON.stringify(user) === '{}' || user == null || user === Object? null :
+        <nav className="nab">
+          <div className="nab-left">
+            <img src={profilePic} alt="Logo" className="imgSmall" />
+            <span className='navbar-heading'>Empowered</span>
+          </div>
+          <div className="nab-right">
+            <a className='nab-item'><Link to='/' target='_self'>Home</Link></a>
+            {JSON.stringify(user) !== '{}' && user != null && <a className='nab-item'><Link to='/record' target='_self'>Record</Link></a>}
+            {JSON.stringify(user) !== '{}' && user != null && <a className='nab-item'><Link to='/report' target='_self'>Report</Link></a>}
+            {JSON.stringify(user) === '{}' || user == null || user === Object ? LoginBtn : null}
+
+            {JSON.stringify(user) === '{}' || user == null || user === Object ? null :
               <>
-              <img src={profilePic} className='imgSmall marginProfile' onClick={toggleMenu}/>
-              
-              <div className='sub-menu-wrap' id="subMenu">
-                <div className='sub-menu'>
-                  <div className='user-info'>
-                    {/* <img src={profilePic} className='profilePic'/> */}
-                    <h5>{userdoc.name}</h5>
+                <img src={profilePic} alt="Profile" className='imgSmall marginProfile' onClick={toggleMenu} />
+
+                <div className='sub-menu-wrap' id="subMenu">
+                  <div className='sub-menu'>
+                    <div className='user-info'>
+                      <h5>{userdoc.name}</h5>
+                    </div>
+                    <hr />
+                    <SignOut />
                   </div>
-                  <hr/>
-                  <SignOut/>
                 </div>
-              </div>
               </>
-}             
-              {/* <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <img src={profilePic} className='imgSmall'/>
-              </a> */}
-          </nav>
-          <Routes>
-              <Route exact path='/' element={<Home/>}></Route>
-              <Route exact path='/record' element={<WebcamVideo/>}></Route>
-              {/* <Route exact path='/record' element={<Webcam2/>}></Route> */}
-              <Route exact path='/report' element={<Report/>}></Route>
-              <Route exact path='/login' element={<SignIn/>}></Route>
-              <Route exact path='/signup' element={<SignUp/>}></Route>
-          </Routes>
+            }
+          </div>
+        </nav>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/record' element={<WebcamVideo />} />
+          <Route exact path='/report' element={<Report />} />
+          <Route exact path='/login' element={<SignIn />} />
+          <Route exact path='/signup' element={<SignUp />} />
+        </Routes>
       </BrowserRouter>
-      {console.log('[navbar] name of user: ', userdoc.name)}
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
+
+
+// import React from 'react'
+// import {useContext} from "react";
+// import {BrowserRouter, Routes, Route, Redirect, Link } from "react-router-dom";
+// import Report from './Report'
+// import Home from './Home'
+// import SignIn from './SignIn'
+// import WebcamVideo from './WebcamVideo';
+// import SignUp from './SignUp';
+// import { LoginContext,  UserContext} from "../AppContext/Context";
+// import SignOut from './SignOut'
+
+// import profilePic from '../../src/images/profile_default.png'
+
+
+// function Navbar(){
+//   const {user,setUser} = useContext(LoginContext); 
+//   const {userdoc,setUserDoc} = useContext(UserContext);
+//   const LoginBtn = (
+//     <button className="btnLogin linkLogin"><Link to='/login' target='_self' className='white'>Login</Link></button>
+//   )
+
+//   const toggleMenu = () => {
+//     let subMenu = document.getElementById('subMenu');
+//     subMenu.classList.toggle("open-menu")
+//   }
+
+//   // TODO: [Batool] add Empowered on left hand side of navbar 
+//   // Problem coming on the above todo
+//   return (
+//     <div className='navbar-wrapper'>
+//       {/* <span className='navbar-heading'>Empowered</span> */}
+//       <BrowserRouter>
+//           <nav className="nab">
+//               <a className='nab-item'><Link to='/' target='_self'>Home</Link></a>
+//               { JSON.stringify(user) !== '{}' && user != null && <a className='nab-item'><Link to='/record' target='_self'>Record</Link></a>}
+//               { JSON.stringify(user) !== '{}' && user != null && <a className='nab-item'><Link to='/report' target='_self'>Report</Link></a>}
+//               {/* { console.log("[Navbar] user: ", user)} */}
+//               {/* { console.log(" JSON.stringify(user) === '{}",  JSON.stringify(user) === '{}')} */}
+//               { JSON.stringify(user) === '{}' || user == null || user === Object? LoginBtn:null }
+              
+//               {JSON.stringify(user) === '{}' || user == null || user === Object? null :
+//               <>
+//               <img src={profilePic} className='imgSmall marginProfile' onClick={toggleMenu}/>
+              
+//               <div className='sub-menu-wrap' id="subMenu">
+//                 <div className='sub-menu'>
+//                   <div className='user-info'>
+//                     <h5>{userdoc.name}</h5>
+//                   </div>
+//                   <hr/>
+//                   <SignOut/>
+//                 </div>
+//               </div>
+//               </>
+// }             
+//               {/* <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+//                 <img src={profilePic} className='imgSmall'/>
+//               </a> */}
+//           </nav>
+//           <Routes>
+//               <Route exact path='/' element={<Home/>}></Route>
+//               <Route exact path='/record' element={<WebcamVideo/>}></Route>
+//               <Route exact path='/report' element={<Report/>}></Route>
+//               <Route exact path='/login' element={<SignIn/>}></Route>
+//               <Route exact path='/signup' element={<SignUp/>}></Route>
+//           </Routes>
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// export default Navbar
 
 // import React from 'react'
 // import {useContext} from "react";
