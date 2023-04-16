e6d17e3b0736ed0fe2e9bc77fb16594360286afb
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-17 02:05:50 +0500

    display scores on analyze button

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 
 function Upload() {
-  const [showScores, setShowScores] = useState(null)
+  const [showScores, setShowScores] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // prevent default form submission behavior
+    setShowScores(true);
+  }
 
   return (
     <div className='padding-horizontal'>
       <h2 className='padding-getstarted centerText'><span className='purple'>Analyze</span> your audio and/or video</h2>
-      <form className='centerText' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <form className='centerText' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} onSubmit={handleSubmit}>
         <div className='paddingFile' style={{ marginRight: '1rem' }}>
           <label>Upload <span className='purple bold'>Audio</span>:</label><span> </span> 
           <input type="file" id="audio" name="audio"/>
@@ -18,9 +23,14 @@ function Upload() {
         <div className='paddingFile' style={{paddingRight : '13%'}}>
           <button className='centerText heartrateBtn analyzeBtn'>Analyze</button>
         </div>
-
-
       </form>
+
+      {showScores && <div className="wrapper padding-scores">
+        <div className="tabs">
+          <div className="tab-content">
+          </div>
+        </div>
+      </div>}
     </div>
   )
 }
