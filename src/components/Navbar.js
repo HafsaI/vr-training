06c43f6b97ca236f619c090772f74cd9b59c89c2
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-21 00:08:22 +0500

    template added for home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,7 @@ import arrow from '../../src/images/arrow.png';
 import GetStarted from './GetStarted';
 import StartSession from './StartSession';
 import Upload from './Upload';
+import Home2 from './Home2';
 
 function Navbar() {
   const { user, setUser } = useContext(LoginContext);
@@ -65,7 +66,8 @@ function Navbar() {
           </div>
         </nav>
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          {/* <Route exact path='/' element={<Home />} /> */}
+          <Route exact path='/' element={<Home2 />} />
           <Route exact path='/record' element={<WebcamVideo />} />
           <Route exact path='/report' element={<Report />} />
           <Route exact path='/login' element={<SignIn />} />
