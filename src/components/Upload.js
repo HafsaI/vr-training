e9b1154656baa3fef5020ab2172d0bb80df60415
HafsaI <hafsairfan80@gmail.com>
2023-04-25 22:13:47 +0500

    Upload Speech File And Get Scores

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function Upload() {
   const [showScores, setShowScores] = useState(false)
+  const [selectedAudio, setSelectedAudio] = useState(null);
+
+  const showSpeechScores = (scores) => {
+    console.log("Individual Speech Scores", scores.data)
+    // console.log("clarity comment", scores.data.clarity_comments)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent default form submission behavior
-    setShowScores(true);
+
+    if (!selectedAudio) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append('audFile', selectedAudio);
+   
+
+    
+    // /* sending audio to backend */
+    // axios.post('https://flask-production-0341.up.railway.app/sendaudio', formData)
+    // .then(function(response) {
+    //   console.log('Reponse',response);
+    // })
+    // .catch(function(error) {
+    //   console.log('Uploading error',error);
+    // });
+    // console.log('Speech Audio File Sent To Backend')
+
+    // /* getting speech scores from backend */
+    // axios.get('https://flask-production-0341.up.railway.app/getscores')
+    // .then(function (response) {
+    //   console.log("Speech scores response", response);
+    //   setShowScores(true);
+    //   showSpeechScores(response)
+
+    // })  
+       
   }
 
   return (
@@ -14,14 +48,15 @@ function Upload() {
       <form className='centerText' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} onSubmit={handleSubmit}>
         <div className='paddingFile' style={{ marginRight: '1rem' }}>
           <label>Upload <span className='purple bold'>Audio</span>:</label><span> </span> 
-          <input type="file" id="audio" name="audio"/>
+          <input type="file" id="audio" name="audio" onChange={(e) => setSelectedAudio(e.target.files[0])}/>
+          
         </div>
         <div className='paddingFile'>
           <label>Upload <span className='purple bold'>Video</span>:</label><span> </span> 
           <input type="file" id="video" name="video"/>
         </div>
         <div className='paddingFile' style={{paddingRight : '13%'}}>
-          <button className='centerText heartrateBtn analyzeBtn'>Analyze</button>
+          <button className='centerText heartrateBtn analyzeBtn'>Analyze Audio</button>
         </div>
       </form>
 
@@ -36,3 +71,15 @@ function Upload() {
 }
 
 export default Upload;
+
+
+ // fetch("http://127.0.0.1:5000/indi", {
+    //   method: 'POST',
+    //   body: formData
+    // })
+    // .then(response => {
+    //   console.log(response);
+    // })
+    // .catch(error => {
+    //   console.error(error);
+    // });
