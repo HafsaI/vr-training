d3c224a0912c8bbea7c1be49ad28299a9b02b549
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-27 23:10:36 +0500

    detailed + quick analysis functionality implemented

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -4,19 +4,19 @@ import Score from './Score'
 function Scores({session, quick = true}) {
   return (
     <div> 
-        <h5>Speech Analysis</h5>
-        <hr/>
-        <Score score={session.clarity_comment} title='Clarity'/>
-        <hr/>
-        <Score score={session.pauses_score} title='No. of Pauses taken'/>
-        <hr/>
-        <Score score={session.pronunciation_score+'%'} title='Pronunciation'/>
-        <hr/>
-        <Score score={session.speakingrate_comment} title='Speaking Rate'/>
-        <hr/>
-        <Score score={session.listenability_score+'%'} title='Listenability'/>
-        { !quick &&
+        { (!quick && session.posture_score) &&
           <>
+            <h5>Speech Analysis</h5>
+            <hr/>
+            <Score score={session.clarity_comment} title='Clarity'/>
+            <hr/>
+            <Score score={session.pauses_score} title='No. of Pauses taken'/>
+            <hr/>
+            <Score score={session.pronunciation_score+'%'} title='Pronunciation'/>
+            <hr/>
+            <Score score={session.speakingrate_comment} title='Speaking Rate'/>
+            <hr/>
+            <Score score={session.listenability_score+'%'} title='Listenability'/>
             <h5 className='analysisType'>Body Language Analysis</h5>
             <hr/>
             <Score score={session.posture_score+'/10'} title='Posture'/>
@@ -28,7 +28,13 @@ function Scores({session, quick = true}) {
             <hr/>
             <Score score={session.nervousness_score+'/10'} title='Nervousness'/> 
           </> 
-        }            
+        }   
+
+        { (!quick && !session.posture_score) &&
+          <>
+            <p className='purple bold centerText'>This may take a few minutes! Please wait.</p>
+          </> 
+        }              
     </div>
   )
 }
