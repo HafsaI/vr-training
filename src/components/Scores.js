7b82efb50597c5774a471b741a1c9238d2d5b556
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-27 23:15:57 +0500

    quick analysis functionality

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -4,7 +4,22 @@ import Score from './Score'
 function Scores({session, quick = true}) {
   return (
     <div> 
-        { (!quick && session.posture_score) &&
+      {quick && 
+        <>
+            <h5>Speech Analysis</h5>
+            <hr/>
+            <Score score={session.clarity_comment} title='Clarity'/>
+            <hr/>
+            <Score score={session.pauses_score} title='No. of Pauses taken'/>
+            <hr/>
+            <Score score={session.pronunciation_score+'%'} title='Pronunciation'/>
+            <hr/>
+            <Score score={session.speakingrate_comment} title='Speaking Rate'/>
+            <hr/>
+            <Score score={session.listenability_score+'%'} title='Listenability'/>
+        </>
+      }
+        { (!quick && session.posture_score) && 
           <>
             <h5>Speech Analysis</h5>
             <hr/>
