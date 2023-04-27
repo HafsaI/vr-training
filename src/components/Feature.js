b33c97733246da778873651daf1042d97240d4ff
batool-ahmed <ba06180@st.habib.edu.pk>
2023-04-27 07:27:36 +0500

    removed problematic icons + not working +  read more link + team members' socials

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -5,9 +5,12 @@ function Feature({title, description, isAnalysis}) {
   return (
     <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" style={{marginTop:'2%'}}>
       <div className="icon-box" data-aos="fade-up" data-aos-delay="100">
-        <div className="icon"><i className='bx bx-dribble'></i></div>
+        {/* <div className="icon"><i className='bx bx-dribble'></i></div> */}
         <h4 className="title">{title}</h4>
-        <p className="description">{description} {isAnalysis?<><br/><a style={{textDecoration:'underline'}}>Read More</a></>:null}</p>
+        <p className="description">{description} 
+          {/* {isAnalysis?<><br/><a style={{textDecoration:'underline'}}>Read More</a></>
+          :null} */}
+        </p>
       </div>
     </div>
   );
--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -6,12 +6,12 @@ function TeamMember( {pic, major='CS', name} ) {
     <div className="member" data-aos="fade-up" data-aos-delay="300">
       <div className="member-img">
         <img src={pic} className="img-fluid" alt=""/>
-        <div className="social">
+        {/* <div className="social">
           <a href=""><i className="bi bi-twitter"></i></a>
           <a href=""><i className="bi bi-facebook"></i></a>
           <a href=""><i className="bi bi-instagram"></i></a>
           <a href=""><i className="bi bi-linkedin"></i></a>
-        </div>
+        </div> */}
       </div>
       <div className="member-info">
         <h4>{name}</h4>
