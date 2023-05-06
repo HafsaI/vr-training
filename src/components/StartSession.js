dc260a4d5f0bb13ad6836586ac8ad4b9ace0a74d
batool-ahmed <ba06180@st.habib.edu.pk>
2023-05-06 23:10:35 +0500

    arduino id field

--- src/components/StartSession.js
+++ src/components/StartSession.js
@@ -4,21 +4,22 @@ import { Link } from 'react-router-dom'
 function StartSession() {
   return (
     <div className='center centerText'>
-      {/* <div>
-        <label forHtml="heart-rate">Current Heartrate:</label> 
-        <input name="heart-rate" readOnly/>
-        <button className='heartrateBtn'>Get Heartrate</button>
+      <div>
+        <label forHtml="heart-rate">Enter Arduino ID:</label> 
+        <input name="heart-rate"/>
+        {/* <input name="heart-rate" readOnly/>
+        <button className='heartrateBtn'>Get Heartrate</button> */}
       </div>
       <br/>
-      <br/> */}
-      <div>
+      <br/>
+      {/* <div>
         <h6>Get your equipment ready</h6>
       </div>
       <br/>
       <div>
         <h6>&</h6>
       </div>
-      <br/>
+      <br/> */}
       <div >
         <p>Start Recording:</p>
         <button className='heartrateBtn noLine'><Link to='/record' target='_self' className=' heartrateBtn noLine'>Open Webcam</Link></button>
