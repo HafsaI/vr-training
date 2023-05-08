5703244a7a3d681b4b0edfe5da2fa957c2e8caec
batool-ahmed <ba06180@st.habib.edu.pk>
2023-05-08 23:36:38 +0500

    table another component

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import darrow from '../images/down-arrow.png'
 import plus from '../images/plus.png'
 import minus from '../images/minus.png'
+import Table from './Table'
 
 function Score({ title, score, showArrow = false}) {
     const [showDescription, setShowDescription] = useState(false)
@@ -37,52 +38,7 @@ function Score({ title, score, showArrow = false}) {
       }
 
       else {
-        setDescription(<table style={{ margin: '0 auto' }} cellPadding='10'>
-        <thead>
-          <tr>
-            <th>Score</th>
-            <th>Description</th>
-            <th>Grade level</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>90.0-100.0</td>
-            <td>Very easy</td>
-            <td>5th grade</td>
-          </tr>
-          <tr>
-            <td>80.0-89.9</td>
-            <td>Easy</td>
-            <td>6th grade</td>
-          </tr>
-          <tr>
-            <td>70.0-79.9</td>
-            <td>Fairly easy</td>
-            <td>7th grade</td>
-          </tr>
-          <tr>
-            <td>60.0-69.9</td>
-            <td>Standard/Plain English</td>
-            <td>8th &amp; 9th grade</td>
-          </tr>
-          <tr>
-            <td>50.0-59.9</td>
-            <td>Fairly difficult</td>
-            <td>10th to 12th grade</td>
-          </tr>
-          <tr>
-            <td>30.0-49.9</td>
-            <td>Difficult</td>
-            <td>College</td>
-          </tr>
-          <tr>
-            <td>0.0-29.9</td>
-            <td>Very difficult</td>
-            <td>College graduate</td>
-          </tr>
-        </tbody>
-      </table>)
+        setDescription(<Table/>)
       }
     }
 
---
+++ src/components/Table.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <table  cellPadding='10' style={{margin: '0 auto', boxShadow:'0px 0px 8px 0px rgba(0,0,0,0.2)'}}>
+        <thead>
+          <tr>
+            <th>Score</th>
+            <th>Description</th>
+            <th>Understandable for which grade level</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>90.0-100.0</td>
+            <td>Very easy</td>
+            <td>5th grade</td>
+          </tr>
+          <tr>
+            <td>80.0-89.9</td>
+            <td>Easy</td>
+            <td>6th grade</td>
+          </tr>
+          <tr>
+            <td>70.0-79.9</td>
+            <td>Fairly easy</td>
+            <td>7th grade</td>
+          </tr>
+          <tr>
+            <td>60.0-69.9</td>
+            <td>Standard/Plain English</td>
+            <td>8th &amp; 9th grade</td>
+          </tr>
+          <tr>
+            <td>50.0-59.9</td>
+            <td>Fairly difficult</td>
+            <td>10th to 12th grade</td>
+          </tr>
+          <tr>
+            <td>30.0-49.9</td>
+            <td>Difficult</td>
+            <td>College</td>
+          </tr>
+          <tr>
+            <td>0.0-29.9</td>
+            <td>Very difficult</td>
+            <td>College graduate</td>
+          </tr>
+        </tbody>
+      </table>
+  )
+}
+
+export default Table
